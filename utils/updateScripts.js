c0ce1947be6ac6e4ef349fad7c360cc5a0ed5ffc
Naeemgg <naeemqg1@gmail.com>
2024-08-22 11:34:03 +0530

    log info of commands

--- utils/updateScripts.js
+++ utils/updateScripts.js
@@ -4,25 +4,30 @@ import path from "path";
 
 export const updateScripts = async () => {
 	const packageJsonPath = path.join(process.cwd(), "package.json");
+
 	fs.readFile(packageJsonPath, "utf8", async (err, data) => {
 		if (err) {
-			console.log(chalk.red("Looks like package.json file is not present in the current directory"))
-			console.log(chalk.green("you can create it with following command:"))
-			console.log(chalk.green("npm init"))
+			console.log(chalk.red("It looks like the package.json file is missing in the current directory."));
+			console.log(chalk.green("To create a new package.json file, you can use the following command:"));
+			console.log(chalk.green("npm init"));
 			console.error(`Error reading package.json: ${err}`);
 			return;
 		}
+
 		let packageJson;
 		try {
+
 			packageJson = JSON.parse(data);
 		} catch (parseErr) {
 			console.error(`Error parsing package.json: ${parseErr}`);
 			return;
 		}
 
+
 		if (!packageJson.scripts) {
 			packageJson.scripts = {};
 		}
+
 
 		packageJson.scripts["db:pull"] = "drizzle-kit introspect";
 		packageJson.scripts["db:push"] = "drizzle-kit push";
@@ -33,14 +38,24 @@ export const updateScripts = async () => {
 		packageJson.scripts["db:check"] = "drizzle-kit check";
 		packageJson.scripts["db:studio"] = "drizzle-kit studio";
 
+
 		const updatedPackageJson = JSON.stringify(packageJson, null, 2);
 
 		fs.writeFile(packageJsonPath, updatedPackageJson, "utf8", (writeErr) => {
 			if (writeErr) {
 				console.error(`Error writing package.json: ${writeErr}`);
 			} else {
-				console.log("package.json has been updated successfully.");
+				console.log(chalk.green("Success! Your package.json file has been updated with the following scripts:"));
+				console.log(chalk.blue("db:pull - Introspects the database schema."));
+				console.log(chalk.blue("db:push - Pushes the latest changes to the database."));
+				console.log(chalk.blue("db:migrate - Applies pending migrations to the database."));
+				console.log(chalk.blue("db:generate - Generates code based on the current database schema."));
+				console.log(chalk.blue("db:drop - Drops the entire database schema."));
+				console.log(chalk.blue("db:up - Brings up the database to the latest version."));
+				console.log(chalk.blue("db:check - Checks the current database schema against the code."));
+				console.log(chalk.blue("db:studio - Opens a database management studio for visual interaction."));
 			}
 		});
 	});
 };
+
